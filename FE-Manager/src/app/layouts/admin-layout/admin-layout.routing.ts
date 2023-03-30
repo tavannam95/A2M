c52b4f2ce36e6d697497dd87ed522bfac4509619
tavannam95 <89097530+tavannam95@users.noreply.github.com>
2023-03-30 16:50:10 +0700

    fe manager

--- FE-Manager/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ FE-Manager/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -8,7 +8,7 @@ import { IconsComponent } from '../../icons/icons.component';
 import { MapsComponent } from '../../maps/maps.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
-import { MovieListComponent } from 'app/share/movie/movie-list/movie-list.component';
+import { MovieListComponent } from 'app/page/movie/movie-list/movie-list.component';
 
 export const AdminLayoutRoutes: Routes = [
     // {
