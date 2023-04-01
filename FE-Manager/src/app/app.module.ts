83d43d32fb1caa8ee22e068e7851e0db1fbdca70
tavannam95 <89097530+tavannam95@users.noreply.github.com>
2023-04-01 17:45:25 +0700

    confirm dialog

--- FE-Manager/src/app/app.module.ts
+++ FE-Manager/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   imports: [
@@ -17,6 +18,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
+    MatDialogModule,
   ],
   declarations: [
     AppComponent,
