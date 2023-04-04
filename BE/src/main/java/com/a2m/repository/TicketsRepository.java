84d7888b872a08ef7effb47ff020ab053956c8e2
tavannam95 <89097530+tavannam95@users.noreply.github.com>
2023-04-04 21:48:43 +0700

    remove

--- BE/src/main/java/com/a2m/repository/TicketsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.a2m.repository;
-
-import com.a2m.entities.Tickets;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketsRepository extends JpaRepository<Tickets,Long> {
-}
